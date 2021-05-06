fd719958256f21f6d42740dfae808e3d1a202bb4
Soyeong Choi <simc2644@gmail.com>
2021-05-07 02:41:35 +0900

    Add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 @Module({
   imports: [
+    ConfigModule.forRoot(
+      isGlobal: true,
+      envFilePath: '.env',
+    ),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'sychoi',
+      // password: '12345',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
