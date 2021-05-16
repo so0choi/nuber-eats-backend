250473a4980ba9518e65f743538fd08b876acd78
Soyeong Choi <simc2644@gmail.com>
2021-05-16 23:26:28 +0900

    Add restaurant update service

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Nuber Eats
 
 Uber Eats 백엔드
+
+## User Model
+
+- id
+- createdAt
+- updatedAt
+- email
+- password
+- role (client/owner/delivery)
+
+## User CRUD
+
+- Created account
+- Log in
+- See profile
+- Edit profile
+- Verify email
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -17,6 +18,19 @@ export class RestaurantResolver {
   ): Promise<boolean> {
     try {
       await this.restaurantService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  @Mutation(returns => Boolean)
+  async updateRestaurant(
+    @Args() updateRestaurantDto: UpdateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.updateRestaurant(updateRestaurantDto);
       return true;
     } catch (e) {
       console.log(e);
